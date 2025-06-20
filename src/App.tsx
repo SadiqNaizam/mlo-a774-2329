56d00084dac0299dd12e5138b5672bec666e4b1c
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 17:38:26 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Ensure this path is correct if sonner is also from ui
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { ThemeProvider } from "./contexts/ThemeContext"; // Adjusted path
 
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import HomePage from "./pages/HomePage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductListingPage from "./pages/ProductListingPage";
-import UserProfilePage from "./pages/UserProfilePage"; // Import the new profile page
+import UserProfilePage from "./pages/UserProfilePage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 
 const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
+<ThemeProvider defaultTheme="system" storageKey="quickdash-ui-theme">
+  <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+      <Toaster />
+      <Sonner /> {/* Assuming this is correctly named and imported */}
+      <BrowserRouter>
+          <Routes>
 
 
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/product-detail" element={<ProductDetailPage />} />
-          <Route path="/product-listing" element={<ProductListingPage />} />
-          <Route path="/profile" element={<UserProfilePage />} /> {/* Add route for profile page */}
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/product-detail" element={<ProductDetailPage />} />
+            <Route path="/product-listing" element={<ProductListingPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
 
 
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
+          </Routes>
+      </BrowserRouter>
+      </TooltipProvider>
+  </QueryClientProvider>
+</ThemeProvider>
 );
 
 export default App;
