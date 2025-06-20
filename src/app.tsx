01407773543bc7958a75a3c579768f6a405b1405
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 16:58:27 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductListingPage from "./pages/ProductListingPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+
+const App = () => (
+<QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+        <Routes>
+
+
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/product-detail" element={<ProductDetailPage />} />
+          <Route path="/product-listing" element={<ProductListingPage />} />
+          {/* catch-all */}
+          <Route path="*" element={<NotFound />} />
+
+
+        </Routes>
+    </BrowserRouter>
+    </TooltipProvider>
+</QueryClientProvider>
+);
+
+export default App;
